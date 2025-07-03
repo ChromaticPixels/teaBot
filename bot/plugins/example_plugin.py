cb53a344b6ebb709663680213c6b370e9a5df232
ChromaticPixels <88055623+ChromaticPixels@users.noreply.github.com>
2025-07-03 05:02:32 +0000

    added word/threes info and rand word cmd

--- bot/plugins/example_plugin.py
+++ bot/plugins/example_plugin.py
@@ -1,5 +1,7 @@
 import crescent
 import hikari
+import random
+
 #from bot.utils import Plugin
 
 plugin = crescent.Plugin[hikari.GatewayBot, None]()
@@ -9,3 +11,17 @@ plugin = crescent.Plugin[hikari.GatewayBot, None]()
 class ping:
     async def callback(self, ctx: crescent.Context) -> None:
         await ctx.respond("Pong!")
+
+@plugin.include
+@crescent.command
+class word:
+    async def callback(self, ctx: crescent.Context) -> None:
+        with open("scrabble_words_24.txt", "r") as wordsFile:
+            randWord = random.choice(wordsFile.readLines)
+        await ctx.respond(randWord)
+
+#@plugin.include
+#@crescent.command
+#class three:
+#    async def callback(self, ctx: crescent.Context) -> None:
+#        await ctx.respond("Pong!")
