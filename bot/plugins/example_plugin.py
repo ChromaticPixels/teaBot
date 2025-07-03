10d3940ea769acfb3f55ad74436f1c895db9139a
ChromaticPixels <88055623+ChromaticPixels@users.noreply.github.com>
2025-07-03 05:42:00 +0000

    fixed rand word, added rand threes

--- bot/plugins/example_plugin.py
+++ bot/plugins/example_plugin.py
@@ -1,10 +1,15 @@
 import crescent
 import hikari
 import random
+import os
+import json
+from collections import Counter
 
 #from bot.utils import Plugin
 
 plugin = crescent.Plugin[hikari.GatewayBot, None]()
+
+os.chdir(f"{os.getcwd()}/bot/plugins")
 
 @plugin.include
 @crescent.command
@@ -17,11 +22,13 @@ class ping:
 class word:
     async def callback(self, ctx: crescent.Context) -> None:
         with open("scrabble_words_24.txt", "r") as wordsFile:
-            randWord = random.choice(wordsFile.readLines)
+            randWord = random.choice(wordsFile.readlines())
         await ctx.respond(randWord)
 
-#@plugin.include
-#@crescent.command
-#class three:
-#    async def callback(self, ctx: crescent.Context) -> None:
-#        await ctx.respond("Pong!")
+@plugin.include
+@crescent.command
+class three:
+    async def callback(self, ctx: crescent.Context) -> None:
+        with open("scrabble_threes_24.json", "r") as threesFile:
+            randThree = ", ".join(str(i) for i in random.choice(Counter(json.load(threesFile)).most_common(1400)))
+        await ctx.respond(randThree)
