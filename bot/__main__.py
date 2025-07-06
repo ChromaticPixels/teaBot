1978e751594ac82426103f3e5440bb1f27215131
ChromaticPixels <88055623+ChromaticPixels@users.noreply.github.com>
2025-07-06 00:56:13 +0000

    expired interaction handling, crescent ctx class

--- bot/__main__.py
+++ bot/__main__.py
@@ -15,8 +15,17 @@ dotenv.load_dotenv()
 
 bot = hikari.GatewayBot(os.environ["TOKEN"])
 
+async def unhandled_comp_hook(inter: hikari.ComponentInteraction) -> None:
+    await inter.create_initial_response(
+        hikari.ResponseType.MESSAGE_CREATE,
+        "Something went wrong, or the interaction expired.",
+        flags=hikari.MessageFlag.EPHEMERAL
+    )
+
 miru_client = miru.Client(bot)
+miru_client.set_unhandled_component_interaction_hook(unhandled_comp_hook)
 model = MyModel(miru_client)
+
 client = crescent.Client(bot, model)
 client.plugins.load_folder("bot.plugins")
 
--- bot/plugins/example_plugin.py
+++ bot/plugins/example_plugin.py
@@ -17,11 +17,16 @@ os.chdir(f"{os.getcwd()}/bot/plugins")
 class ContextView(miru.View):
     def __init__(self, ctx: crescent.Context, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
-        self.players = 0
         self.crescent_ctx = ctx
 
+# view for tea games
+class TeaView(ContextView):
+    def __init__(self, ctx: crescent.Context, *args, **kwargs) -> None:
+        super().__init__(ctx, *args, **kwargs)
+        self.players = 0
+
     # define a new TextSelect menu with two options (vestigal template that might be useful)
-    @miru.text_select(
+    '''@miru.text_select(
         placeholder="Select me!",
         options=[
             miru.SelectOption(label="Option 1"),
@@ -29,25 +34,31 @@ class ContextView(miru.View):
         ],
     )
     async def basic_select(self, ctx: miru.ViewContext, select: miru.TextSelect) -> None:
-        await ctx.respond(f"You've chosen {select.values[0]}!")
+        await ctx.respond(f"You've chosen {select.values[0]}!")'''
 
     # join tea (TODO: once per user; currently inf per user)
-    @miru.button(label="Players: 0", style=hikari.ButtonStyle.PRIMARY)
-    async def basic_button(self, ctx: miru.ViewContext, button: miru.Button) -> None:
+    @miru.button(
+        label="Players: 0",
+        style=hikari.ButtonStyle.PRIMARY
+    )
+    async def join_button(self, ctx: miru.ViewContext, button: miru.Button) -> None:
         self.players += 1
         button.label = f"Players: {self.players}"
         await ctx.edit_response(components=self)
         await ctx.respond("Joined!", flags=hikari.MessageFlag.EPHEMERAL)
 
-    # cancel view
-    @miru.button(label="Abort!", style=hikari.ButtonStyle.DANGER)
+    # cancel tea
+    @miru.button(
+        label="Abort!",
+        style=hikari.ButtonStyle.DANGER
+    )
     async def stop_button(self, ctx: miru.ViewContext, button: miru.Button) -> None:
         await ctx.respond("The host has aborted.")
         self.stop()
 
     async def on_timeout(self) -> None:
-        # no interactions so no ctx available to respond with...
-        if self.players == 0:
+        # if players == 0, no interactions so no ctx available to respond with...
+        if self.message is None:
             # ...thus, moderate scuff
             await self.crescent_ctx.respond("Nobody joined? How drab...")
             return None
@@ -56,7 +67,7 @@ class ContextView(miru.View):
 @plugin.include
 @crescent.command
 async def ping(ctx: crescent.Context) -> None:
-    view = ContextView(ctx, timeout=3.0)
+    view = TeaView(ctx, timeout=3.0)
     await ctx.respond("Pong!", components=view)
     ctx.client.model.miru.start_view(view)
 
